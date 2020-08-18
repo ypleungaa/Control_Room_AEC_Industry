2d75fdbf2922bba611a47f7c3d6aefdb8cb34aa0
ypleungaa <56892276+ypleungaa@users.noreply.github.com>
2020-08-19 01:50:58 +0800

    updated

---
+++ Code/Dashboard/Safety/safety.sql
@@ -0,0 +1,17 @@
+CREATE TABLE table5 (
+    id int,
+    location varchar(255),
+    investigation varchar(255),
+	investigation_level varchar(255), 
+	date_of_accident date,
+	date_of_report date,
+	time_of_accident time,
+	activity varchar(255),
+	ai_category varchar(255),
+	body_part varchar(255),
+	nature_of_injury varchar(255),
+	type_of_contact varchar(255),
+	no_of_days_lost int,
+	target_closure_date date,
+	actual_closure_date date
+);
